a67932f0f4a90ad2e9719311856f04328195fd0f
NivyaS2k15 <148306006+NivyaS2k15@users.noreply.github.com>
2025-08-06 22:13:01 +0530

    first commit

---
+++ demo/assignment2.ino
@@ -0,0 +1,60 @@
+/* Sweep
+ASSIGNMETN NIVYA TWO
+ by BARRAGAN <http://barraganstudio.com>
+ This example code is in the public domain.
+
+ modified 8 Nov 2013
+ by Scott Fitzgerald
+ https://www.arduino.cc/en/Tutorial/LibraryExamples/Sweep
+*/
+
+#include <Servo.h>
+
+Servo myservo;  // create Servo object to control a servo
+// twelve Servo objects can be created on most boards
+
+int pos = 0;    // variable to store the servo position
+const int buttonPin = 2;
+int buttonFirstState = 0;
+int buttonLastState = 1;
+int count = 0;
+ 
+void setup() {
+  myservo.attach(9); 
+  pinMode(buttonPin, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+}
+   // attaches the servo on pin 9 to the Servo object
+
+
+void loop() {
+  buttonFirstState = digitalRead(buttonPin);  // read the button
+ delay(50); // debounce delay
+  if (buttonFirstState == HIGH && buttonLastState == LOW) {
+    // If button is pressed and servo is not at 180, move to 180
+    for (pos = 0; pos <= 180; pos++) {
+      myservo.write(pos);
+      delay(10);
+    }
+   buttonLastState = HIGH;
+    
+  }
+ 
+
+  if  (buttonFirstState == LOW && buttonLastState == HIGH) {
+    // If button is released and servo is at 180, move back to 0
+    for (pos = 180; pos >= 0; pos--) {
+      myservo.write(pos);
+      delay(10);
+    }
+   buttonLastState = LOW ;
+    count = count +1;
+  }
+
+     if (count % 2 != 0) {
+    digitalWrite(LED_BUILTIN, HIGH); // LED ON for odd press count
+  } else {
+    digitalWrite(LED_BUILTIN, LOW);  // LED OFF for even
+  }
+}
+
---
+++ demo/ledproblem.ino
@@ -0,0 +1,48 @@
+void setup() {
+  // initialize digital pin LED_BUILTIN as an output.
+  pinMode(1, OUTPUT);
+  pinMode(2, OUTPUT);
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, OUTPUT);
+  pinMode(8, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(1, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(1, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+  digitalWrite(2, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(2, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+   digitalWrite(3, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(3, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+   digitalWrite(4, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(4, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+   digitalWrite(5, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(5, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+   digitalWrite(6, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(6, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);
+    digitalWrite(7, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(7, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);   
+  digitalWrite(8, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);                      // wait for a second
+  digitalWrite(8, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000); 
+                        // wait for a second
+}
