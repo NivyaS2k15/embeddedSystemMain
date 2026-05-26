b228797be943072405edfcf876855708b892d022
NivyaS2k15 <148306006+NivyaS2k15@users.noreply.github.com>
2026-05-26 16:18:42 +0530

    new

---
+++ stm32Barematel/PROECT_NEW/LED_P/Debug/Src/main.d
@@ -0,0 +1,7 @@
+Src/main.o: ../Src/main.c \
+ C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx.h \
+ C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx_gpio_driver.h \
+ C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx.h
+C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx.h:
+C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx_gpio_driver.h:
+C:/embeddedSystem/stm32Barematel/PROECT_NEW/LED_P/drivers/inc/stm32f446xx.h:
